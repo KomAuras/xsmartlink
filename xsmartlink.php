08561479ca74f3f92a844f423fd5d6b493eafcf8
Evgeny Stefanenko <mixer@clarionlife.net>
2017-09-20 11:25:53 +0000

    xsmartlink.php отредактирован онлайн на Bitbucket

--- xsmartlink.php
+++ xsmartlink.php
@@ -3,7 +3,7 @@
  * Plugin Name:       xSmart Link
  * Plugin URI:        http://www.claruionlife.net
  * Description:       Smart link posts plugin
- * Version:           2.0.0
+ * Version:           2.0.1
  * Author:            Evgeny Stefanenko
  * Author URI:        http://www.claruionlife.net
  * License:           GPL-2.0+
